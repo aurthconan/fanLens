9622ecf01d3ae377c9429931a726ba528626d246
aurthconan <aurthconan@gmail.com>
2013-08-21 17:14:57 +0800

    1. Modify make file to search for all cpp file 2. Add fanVector test

--- test/fanVector_test.cpp
+++ test/fanVector_test.cpp
@@ -1,31 +1,29 @@
-
 #include <gtest/gtest.h>
 
-#include <fanTriple.h>
+#include <fanVector.h>
 
 using namespace fan;
 
-TEST(fanTriple, Constructor_int) {
-    fanTriple<int> _variable;
+TEST(fanVector,Constructor_int) {
+    fanVector<int> _variable;
     EXPECT_EQ( 0, _variable.x );
     EXPECT_EQ( 0, _variable.y );
     EXPECT_EQ( 0, _variable.z );
 
-    fanTriple<int> _variable2( 1, 2, 3 );
+    fanVector<int> _variable2( 1, 2, 3 );
     EXPECT_EQ( 1, _variable2.x );
     EXPECT_EQ( 2, _variable2.y );
     EXPECT_EQ( 3, _variable2.z );
 }
 
-TEST(fanTriple, Constructor_float) {
-    fanTriple<float> _variable;
+TEST(fanVector,Constructor_float) {
+    fanVector<float> _variable;
     EXPECT_FLOAT_EQ( .0f, _variable.x );
     EXPECT_FLOAT_EQ( .0f, _variable.y );
     EXPECT_FLOAT_EQ( .0f, _variable.z );
 
-    fanTriple<float> _variable2( .1f, .2f, .3f );
+    fanVector<float> _variable2( .1f, .2f, .3f );
     EXPECT_FLOAT_EQ( .1f, _variable2.x );
     EXPECT_FLOAT_EQ( .2f, _variable2.y );
     EXPECT_FLOAT_EQ( .3f, _variable2.z );
 }
-

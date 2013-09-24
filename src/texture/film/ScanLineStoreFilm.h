00c2478dd90f230c3a03ebe696e6f9332f2568a7
aurthconan <aurthconan@gmail.com>
2013-09-24 16:20:08 +0800

    Add ScanLineStoreFilm for compute scanline

---
+++ src/texture/film/ScanLineStoreFilm.h
@@ -0,0 +1,25 @@
+#ifndef SCANLINESTOREFILM_H
+
+#define SCANLINESTOREFILM_H
+
+#include <fanFilm.h>
+
+// a class use to store and generate scanline
+class ScanLineStoreFilm
+    : public fan::fanFilm
+{
+public:
+    ScanLineStoreFilm( const fan::fanVector<int, 2>& dimensions );
+    ~ScanLineStoreFilm();
+
+    void setPixel( const fan::fanVector<int, 2>& index,
+                   const fan::fanPixel& pixel );
+
+    void reset();
+
+    int mYMin;
+    int mYMax;
+    fan::fanVector<int, 2>* mpYBucket;
+};
+
+#endif /* end of include guard: SCANLINESTOREFILM_H */
